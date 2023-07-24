34a7c71cd415e9c5eb35d0257157215bdee59b74
Sebastian Konowrocki <sebastian.konowrocki@leancode.pl>
2023-07-24 16:20:54 +0200

    Migrate to nx@latest

--- packages/api-proxy/jest.config.js
+++ packages/api-proxy/jest.config.js
@@ -1,14 +1,15 @@
 module.exports = {
     displayName: "api-proxy",
     preset: "../../jest.preset.js",
-    globals: {
-        "ts-jest": {
-            tsconfig: "<rootDir>/tsconfig.spec.json",
-        },
-    },
+    globals: {},
     testEnvironment: "node",
     transform: {
-        "^.+\\.[tj]sx?$": "ts-jest",
+        "^.+\\.[tj]sx?$": [
+            "ts-jest",
+            {
+                tsconfig: "<rootDir>/tsconfig.spec.json",
+            },
+        ],
     },
     moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
     coverageDirectory: "../../coverage/packages/api-proxy",
